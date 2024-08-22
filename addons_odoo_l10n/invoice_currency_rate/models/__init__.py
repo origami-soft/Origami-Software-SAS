04b3decd4aa3d3d715c66a8c7de8853cdc07fedb
hector-origami <hestevez.origami@gmail.com>
2024-08-22 18:49:58 -0500

    [UPD] Pre-Migracion

--- addons_odoo_l10n/invoice_currency_rate/models/__init__.py
+++ addons_odoo_l10n/invoice_currency_rate/models/__init__.py
@@ -3,5 +3,6 @@
 from . import account_move
 from . import account_move_line
 from . import res_currency
+from . import res_currency_rate
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
