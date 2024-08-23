895cb137984acbec462e326d7c9cec9cb6851869
hector-origami <hestevez.origami@gmail.com>
2024-08-22 20:00:18 -0500

    [UPD] Pre-Migracion

---
+++ addons_odoo_l10n_ar/l10n_ar/migrations/17.0.0.0/pre-migration.py
@@ -0,0 +1,11 @@
+from odoo import api, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    cr.execute("""
+        DELETE FROM res_country WHERE code IS NULL OR code = 'f' OR code = '';
+    """)
+
+    cr.commit()
