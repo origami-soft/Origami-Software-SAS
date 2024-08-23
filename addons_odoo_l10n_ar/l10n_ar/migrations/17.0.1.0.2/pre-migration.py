4520b8cbda9bc995281a989bcacc3675e9c265cc
hector-origami <hestevez.origami@gmail.com>
2024-08-22 20:21:33 -0500

    [UPD] Pre-Migracion

--- addons_odoo_l10n_ar/l10n_ar/migrations/17.0.1.0.2/pre-migration.py
+++ addons_odoo_l10n_ar/l10n_ar/migrations/17.0.1.0.2/pre-migration.py
@@ -1,22 +1,27 @@
-from odoo import api, SUPERUSER_ID
-import logging
-from odoo.upgrade import util
+#!/usr/bin/env python
+# coding: utf-8
 
-_logger = logging.getLogger(__name__)
+
+from odoo import api, SUPERUSER_ID
+
+# import logging
+# from odoo.upgrade import util
+
+# _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
-    # env = api.Environment(cr, SUPERUSER_ID, {})
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    cr.execute("""
+        DELETE FROM res_country WHERE code IS NULL OR code = 'f' OR code = '';
+    """)
+
+    cr.commit()
+
+    # env = util.env(cr)
     #
-    # cr.execute("""
-    #     DELETE FROM res_country WHERE code IS NULL OR code = 'f' OR code = '';
-    # """)
+    # countrys = env["res.country"].sudo().search([('code', '=', False)])
+    # for country in countrys:
+    #     country.unlink()
     #
-    # cr.commit()
-
-    env = util.env(cr)
-
-    countrys = env["res.country"].sudo().search([('code', '=', False)])
-    for country in countrys:
-        country.unlink()
-
-    _logger.info("YUYU Updated %s countrys", len(countrys))
+    # _logger.info("YUYU Updated %s countrys", len(countrys))
