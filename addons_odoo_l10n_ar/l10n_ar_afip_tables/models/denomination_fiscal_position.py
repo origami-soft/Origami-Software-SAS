610e38170323946fa2ce134d1f51e44641617ef5
hector-origami <hestevez.origami@gmail.com>
2024-09-20 01:48:11 -0500

    [UPD] Post-Migracion

--- addons_odoo_l10n_ar/l10n_ar_afip_tables/models/denomination_fiscal_position.py
+++ addons_odoo_l10n_ar/l10n_ar_afip_tables/models/denomination_fiscal_position.py
@@ -8,16 +8,19 @@ class DenominationFiscalPosition(models.Model):
     _name = 'denomination.fiscal.position'
     _description = 'Denominación de posiciones fiscales'
 
-    issue_fiscal_position_id = fields.Many2one(
-        'ar.fiscal.position',
-        'Posicion Fiscal emisora',
-        required=True
-    )
-    receipt_fiscal_position_id = fields.Many2one(
-        'ar.fiscal.position',
-        'Posicion Fiscal receptora',
-        required=True
-    )
+    # issue_fiscal_position_id = fields.Many2one(
+    #     'ar.fiscal.position',
+    #     'Posicion Fiscal emisora',
+    #     required=True
+    # )
+    # receipt_fiscal_position_id = fields.Many2one(
+    #     'ar.fiscal.position',
+    #     'Posicion Fiscal receptora',
+    #     required=True
+    # )
+    issue_fiscal_position_id = fields.Integer()
+    receipt_fiscal_position_id = fields.Integer()
+
     account_denomination_id = fields.Many2one(
         'account.denomination',
         'Denominacion',
