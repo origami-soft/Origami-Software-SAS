b12cfb5745e78462b1d7299201127cbbee54b188
hector-origami <hestevez.origami@gmail.com>
2024-09-20 00:10:49 -0500

    [UPD] Migracion

--- addons_odoo_l10n_ar/l10n_ar/migrations/17.0.1.0/post-migration.py
+++ addons_odoo_l10n_ar/l10n_ar/migrations/17.0.1.0/post-migration.py
@@ -2,26 +2,6 @@ from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
 
-    # Eliminar referencias huérfanas en ir_model_fields
-    # cr.execute("""
-    #     DELETE FROM ir_model_fields
-    #     WHERE model = 'retention.retention.template';
-    # """)
-
-    # # Eliminar datos de selección huérfanos en ir_model_data
-    # cr.execute("""
-    #     DELETE FROM ir_model_data
-    #     WHERE name LIKE 'l10n_ar.selection__retention_retention_template__%';
-    # """)
-
-    # Eliminar posibles referencias en cualquier otra tabla relevante
-    # cr.execute("""
-    #     DELETE FROM ir_model_fields_selection
-    #     WHERE field_id IN (
-    #         SELECT id FROM ir_model_fields WHERE model = 'retention.retention.template'
-    #     );
-    # """)
-
 
     cr.execute("""
             DELETE FROM ir_model_data
@@ -40,5 +20,65 @@ def migrate(cr, version):
             WHERE field_id IN (
                 SELECT id FROM ir_model_fields 
                 WHERE model = 'retention.retention.template'
+            );
+        """)
+
+    cr.execute("""
+            DELETE FROM ir_model_data
+            WHERE model = 'perception.perception.template';
+        """)
+
+    # Eliminar campos de selección relacionados en ir_model_fields
+    cr.execute("""
+            DELETE FROM ir_model_fields
+            WHERE model = 'perception.perception.template';
+        """)
+
+    # Eliminar cualquier campo dependiente en otras tablas
+    cr.execute("""
+            DELETE FROM ir_model_fields_selection
+            WHERE field_id IN (
+                SELECT id FROM ir_model_fields 
+                WHERE model = 'perception.perception.template'
+            );
+        """)
+
+    cr.execute("""
+            DELETE FROM ir_model_data
+            WHERE model = 'account.tax.ar.template';
+        """)
+
+    # Eliminar campos de selección relacionados en ir_model_fields
+    cr.execute("""
+            DELETE FROM ir_model_fields
+            WHERE model = 'account.tax.ar.template';
+        """)
+
+    # Eliminar cualquier campo dependiente en otras tablas
+    cr.execute("""
+            DELETE FROM ir_model_fields_selection
+            WHERE field_id IN (
+                SELECT id FROM ir_model_fields 
+                WHERE model = 'account.tax.ar.template'
+            );
+        """)
+
+    cr.execute("""
+            DELETE FROM ir_model_data
+            WHERE model = 'account.tax.template';
+        """)
+
+    # Eliminar campos de selección relacionados en ir_model_fields
+    cr.execute("""
+            DELETE FROM ir_model_fields
+            WHERE model = 'account.tax.template';
+        """)
+
+    # Eliminar cualquier campo dependiente en otras tablas
+    cr.execute("""
+            DELETE FROM ir_model_fields_selection
+            WHERE field_id IN (
+                SELECT id FROM ir_model_fields 
+                WHERE model = 'account.tax.template'
             );
         """)
